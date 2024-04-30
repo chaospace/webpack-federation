e4eca2dc4f892d7fbb823b9db412f95217f1302f
chaospace <ichaospace@gmail.com>
2024-04-30 10:43:52 +0900

    [SETTING] axios 추가

--- packages/remote/jest.config.js
+++ packages/remote/jest.config.js
@@ -1,16 +1,22 @@
+const { transform } = require("typescript");
+
 const config = {
-  vervose: true,
+  verbose: true,
   rootDir: ".",
   roots: ["<rootDir>/src/", "<rootDir>/tests/"],
-  testEnvironment: "jsdom",
+  // testEnvironment: "node",
   transformIgnorePatterns: ["/node_modules/", "/dist/", "\\.pnp\\.[^\\/]+$"],
   coveragePathIgnorePatterns: ["src/index.ts"],
-  setupFiles: ["./tests/setup.ts"],
+  setupFilesAfterEnv: ["./tests/setup.ts"],
   testMatch: [
     "<rootDir>/src/**/*.(test|spec).(ts|tsx)",
     "<rootDir>/tests/**/*.(test|spec).(ts|tsx)",
   ],
+  transform: {
+    "^.+\\.tsx?$": "babel-jest",
+  },
   moduleNameMapper: {
+    "^@/(.+)$": "<rootDir>/src/$1",
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/tests/__mocks__/fileMock.ts",
     "\\.(css|less)$": "<rootDir>/tests/__mocks__/styleMock.ts",
